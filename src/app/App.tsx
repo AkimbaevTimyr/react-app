69813373f74e985f784f9b8eb00ffe1224e36197
timur <akimbaev.tim2002@gmail.com>
2024-04-15 10:29:46 +0500

    add navbar widgets, create appLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,17 @@
-import { Route, Routes } from "react-router-dom";
 import './styles/index.scss'
-import { Link } from "react-router-dom";
-import { MainPage } from "pages/MainPage";
-import { Suspense} from "react";
 import { useTheme } from "app/providers/ThemeProvider/";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, toogleTheme } = useTheme();
 
     return(
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toogleTheme}>toogle</button>
-            <Link to={'/'}>
-                Главная
-            </Link>
-            <Link to={'/about'}>
-                About
-            </Link>
-            <Suspense fallback={<div>Loading ...</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                </Routes>
-            </Suspense>
-
+            <AppRouter />
         </div>
     )
 }
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import styles from './Navbar.module.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { FC } from 'react';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar: FC<NavbarProps> = ({className}) => {
+    return (
+        <div className={classNames(styles.navbar, {}, [className])}>
+            <div className={classNames(styles.links)}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={classNames(styles.mainLink)}>Главная</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>About</AppLink>
+            </div>
+        </div>
+    )
+}
